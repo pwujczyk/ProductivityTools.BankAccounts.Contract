2cb76e44ae371669a4ee73ab0cbbf7e3172e4185
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-09-25 15:51:29 +0200

    endpoint name

---
+++ ProductivityTools.BankAccounts.Contract/EndpointNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.BankAccounts.Contract
+{
+    public static class EndpointNames
+    {
+        public const string AccountControllerName= "Account";
+        public const string BasicDataMethodName= "BasicData";
+        public const string SaveTransactionsMethodName = "SaveTransactionsMethodName";
+    }
+}
